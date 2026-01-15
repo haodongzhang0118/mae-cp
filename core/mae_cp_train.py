a58fbceeedcb899a32ed9d24cd803f024001dd1b
Haodong Zhang <haodoz0118@gmail.com>
2026-01-15 15:48:51 -0500

    CHanged the log

--- core/mae_cp_train.py
+++ core/mae_cp_train.py
@@ -479,7 +479,7 @@ def train_mae_cp(
         default_root_dir=str(output_path),
         num_sanity_val_steps=num_sanity_val_steps,
         limit_val_batches=limit_val_batches,
-        log_every_n_steps=1, 
+        log_every_n_steps=0, 
         enable_checkpointing=True,
     )
     
